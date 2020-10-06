68a9444f781ec3a60767402ff6b8d3d7b8c7fbfb
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-06 07:37:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+     enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
-end
+    get '/' do
+      @session = session
+      erb :index 
+    end
+
+  helpers do 
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end	  
+
+
+  # def logged_in?
+  #     !!session[:user_id]
+  #   end
+
+  #   def current_user
+  #     User.find(session[:user_id])
+  #     end
+  #   end
+end 
